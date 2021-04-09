c42359cdb5af9cca6c582d67b653bbda231d2240
vpec <victorpenasco1@gmail.com>
2021-04-10 00:45:19 +0200

    [restapi] initial spring boot project

---
+++ helloworld-rest/src/main/java/com/example/helloworldrest/HelloworldRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldRestApplication.class, args);
+	}
+
+}
---
+++ helloworld-rest/src/test/java/com/example/helloworldrest/HelloworldRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
