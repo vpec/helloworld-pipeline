67f4e1dbb8a2554ae965fa292ff321bdf4ea2723
vpec <victorpenasco1@gmail.com>
2021-04-14 00:07:58 +0200

    [develop] change endpoint to /

--- helloworld-rest/src/main/java/com/example/helloworldrest/controller/HelloworldController.java
+++ helloworld-rest/src/main/java/com/example/helloworldrest/controller/HelloworldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloworldController {
  
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello() {
         return "Hello World!";
     }  
